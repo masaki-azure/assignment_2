b78895228af86826ed88ac2ee412f15332f1712b
masaki-azure <masaki.azu.re@outlook.com>
2026-09-22 10:49:31 +0900

    chore: pin provider versions

--- azure/versions.tf
+++ azure/versions.tf
@@ -1,16 +1,19 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
+  }
+
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
 
 provider "azurerm" {
   features {}
+  storage_use_azuread = true
 }
